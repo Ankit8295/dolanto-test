1e279f5e9526b107568dc2d48d581136bdddc429
AnkitRsys <146825879+Ankitrsys@users.noreply.github.com>
2023-11-05 11:31:32 +0530

    setup4

--- src/lib/queries.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export async function getData() {
-  const res = await axios({
-    url: `http://localhost:3001/api/getData`,
-    method: "GET",
-  });
-  return res.data;
-}
