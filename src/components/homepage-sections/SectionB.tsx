6869a15249d1cfeb533029ce0236e3fb51c8e913
vanityadav <vanit.yadav@revnue.com>
2023-11-06 22:29:39 +0530

    Update SectionB.tsx

--- src/components/homepage-sections/SectionB.tsx
+++ src/components/homepage-sections/SectionB.tsx
@@ -10,8 +10,8 @@ import { homeAboutCards } from "@/src/constants/homepage";
 
 export default function SectionB() {
   return (
-    <div className="grid relative grid-cols-2 max-lg:grid-cols-1 max-lg:grid-rows-[auto]  gap-5">
-      <div className="w-full  relative rounded-3xl max-lg:row-start-2">
+    <div className="flex flex-col lg:grid grid-cols-4 grid-rows-3  grid-flow-col">
+      <div className="max-lg:order-2 col-start-1 col-end-3 row-start-1 row-end-4">
         <Image
           src={aboutImg}
           priority={true}
@@ -20,23 +20,8 @@ export default function SectionB() {
           alt={"dolanto about image"}
           className="object-fill  h-full w-full rounded-3xl"
         />
-        {/* {homeAboutCards
-          .filter((card, i) => i === 0)
-          .map((textCard) => (
-            <div
-              key={textCard.title}
-              className="bg-lightBLue max-lg:hidden   absolute bottom-0 right-0 max-w-[31%] max-lg:max-w-[48%]  max-lg:gap-0 max-lg:grow flex flex-col items-start gap-2 rounded-3xl p-6 "
-            >
-              <GradientText customTailwindStyle="text-5xl">
-                {textCard?.title}
-              </GradientText>
-              <GradientText>{textCard?.subTitle}</GradientText>
-              <span className="text-xs">{textCard?.description}</span>
-            </div>
-          ))} */}
       </div>
-
-      <div className="w-full  h-[70%] flex flex-col gap-5  items-start justify-between max-lg:row-start-1">
+      <div className="max-lg:order-1 col-start-3 col-end-5 row-start-1 row-end-3">
         <div className="pl-6 max-lg:pl-0">
           <BlueButtonWrapper>About Us</BlueButtonWrapper>
         </div>
@@ -59,59 +44,47 @@ export default function SectionB() {
             Download Brochure
           </BlueButtonWrapper>
         </div>
+      </div>
 
-        <div className="flex w-full max-lg:hidden absolute p-3 rounded-3xl bottom-0 right-0 z-10 bg-white max-h-[35%] max-w-[75%] gap-5 max-lg:gap-0 max-lg:gap-y-5 max-lg:justify-between max-lg:flex-wrap ">
-          {homeAboutCards
-            .filter((card, i) => i === 0)
-            .map((textCard) => (
-              <div
-                key={textCard.title}
-                className="bg-lightBLue hidden max-lg:flex  w-full  max-lg:gap-0 max-lg:grow flex-col items-start gap-2 rounded-3xl p-3 "
-              >
-                <GradientText customTailwindStyle="text-5xl  max-lg:text-2xl font-medium">
-                  {textCard?.title}
-                </GradientText>
-                <GradientText>{textCard?.subTitle}</GradientText>
-                <span className="text-xs">{textCard?.description}</span>
-              </div>
-            ))}
-          {homeAboutCards.map((textCard) => (
-            <div
-              key={textCard.title}
-              className={`bg-lightBLue  max-w-[25%]   max-lg:max-w-[48%]  max-lg:gap-0 max-lg:grow flex flex-col items-start gap-2 rounded-3xl p-3`}
-            >
-              <GradientText customTailwindStyle="text-5xl  max-lg:text-2xl font-medium">
-                {textCard?.title}
-              </GradientText>
-              <GradientText>{textCard?.subTitle}</GradientText>
-              <span className="text-xs">{textCard?.description}</span>
-            </div>
-          ))}
-          <div className="bg-lightBLue max-w-[25%]   max-lg:max-w-full max-lg:grow flex flex-col items-start gap-2 rounded-3xl p-3  ">
-            <span className="text-xs">
-              “Exceptional Precision and Quality! Dolanto’s clean room solution
-              exceeded our expectations. Their meticulous attention to detail
-              and unwavering commitment to delivering a clean room environment
-              truly impressed us.”
-            </span>
-            <div className="flex gap-2">
-              <Image
-                width={20}
-                height={20}
-                priority={true}
-                quality={100}
-                src={reviewAboutImg}
-                alt="review person image"
-                className=" object-fill"
-              />
-              <div className="flex flex-col items-start">
-                <span className="text-xs">Sarah Johnson</span>
-                <span className="text-xs">Managing Director at L&T</span>
-              </div>
+      <div className="max-lg:order-3 col-start-2 col-end-5 row-start-3 row-end-4 lg:flex gap-5 grid grid-cols-2  bg-white lg:pl-5 lg:pt-5 rounded-t-3xl">
+        {homeAboutCards.map((textCard) => (
+          <div
+            key={textCard.title}
+            className={`bg-lightBLue  flex flex-col items-start gap-2 rounded-3xl p-3 flex-1`}
+          >
+            <GradientText customTailwindStyle="text-5xl  max-lg:text-2xl font-medium">
+              {textCard?.title}
+            </GradientText>
+            <GradientText>{textCard?.subTitle}</GradientText>
+            <span className="text-xs">{textCard?.description}</span>
+          </div>
+        ))}
+        <div className="bg-lightBLue max-w-[25%]   max-lg:max-w-full max-lg:grow flex flex-col items-start gap-2 rounded-3xl p-3 col-span-2 ">
+          <span className="text-xs">
+            “Exceptional Precision and Quality! Dolanto’s clean room solution
+            exceeded our expectations. Their meticulous attention to detail and
+            unwavering commitment to delivering a clean room environment truly
+            impressed us.”
+          </span>
+          <div className="flex gap-2">
+            <Image
+              width={20}
+              height={20}
+              priority={true}
+              quality={100}
+              src={reviewAboutImg}
+              alt="review person image"
+              className=" object-fill"
+            />
+            <div className="flex flex-col items-start">
+              <span className="text-xs">Sarah Johnson</span>
+              <span className="text-xs">Managing Director at L&T</span>
             </div>
           </div>
         </div>
       </div>
+      <div className="col-start-2 row-start-2 w-full h-full rounded-3xl  shadow-[20px_20px_white] hidden lg:block "></div>
+      <div className="col-start-1 row-start-3 w-full h-full rounded-3xl  shadow-[20px_20px_white] hidden lg:block"></div>
     </div>
   );
 }
