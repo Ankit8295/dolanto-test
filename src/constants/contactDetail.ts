c8612c7b5b76e257c59a48444dc52a4f61d9500e
Ankit Kumar <112799319+Ankit8295@users.noreply.github.com>
2023-11-23 12:28:53 +0530

    Update contactDetail.ts

--- src/constants/contactDetail.ts
+++ src/constants/contactDetail.ts
@@ -1,5 +1,5 @@
 export const aboutDolantoContact = {
-  phone: "+ 7 495 129-36-54",
-  address: "Moscow, Zubarev lane, d. 7",
-  email: "support@testbook.com",
+  phone: "+91 75062 38351",
+  address: "303,3rd floor bhoomi velocity plot no. B-39A, B-39A1 Road no 23 wagle estate thane-400604",
+  email: "hr@dolanto.com",
 };
